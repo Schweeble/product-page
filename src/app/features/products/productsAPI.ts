e6d71ffd0bab7c3d52c01227d424045b987235e1
Andrew Franco <schweebledev@gmail.com>
2025-02-19 23:02:59 -0800

    deploy to pages

--- src/app/features/products/productsAPI.ts
+++ src/app/features/products/productsAPI.ts
@@ -1,7 +1,7 @@
 import { Product } from "../../../types/productTypes";
 
 export default function fetchProducts() {
-  return fetch("/data.json")
+  return fetch("./data.json")
     .then((data) => data.json())
     .then((json) => {
       return json as Product[];
